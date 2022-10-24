40ef40b4170c2ed605bf55db2e0ce79132285aaf
Bartosz Błaszczyk <hdmenpl@gmail.com>
2022-10-24 14:07:55 +0200

    final :(

--- Zadanie1WPF/MainWindow.xaml.cs
+++ Zadanie1WPF/MainWindow.xaml.cs
@@ -25,8 +25,10 @@ namespace Zadanie1WPF
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
+        #region FieldsAndVariables
         private string _publicName = "";
         public string PublicName
         {
@@ -60,7 +62,9 @@ namespace Zadanie1WPF
         public bool nullName;
 
         public bool nullAge = true;
+        #endregion
 
+        #region ButtonWithoutBidning
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string name = TextBoxName.Text;
@@ -71,53 +75,55 @@ namespace Zadanie1WPF
             Validation(name, age);
 
         }
+        #endregion
 
+        #region BindingButton
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             AgeValidation(PublicAge);
             NameValidation(PublicName);
             Validation(PublicName, PublicAge);
         }
+        #endregion
 
+        #region Validacja 
         public void Validation(string userName, string userAge)
         {
 
 
-
-          
-                if (!NameValidation(userName))
-                {
+            if (!NameValidation(userName))
+            {
                 Alert.Text = " ";
                 result.Text = " ";
                 Alert.Text = "Błędne dane(imie)";
-                    Alert.Foreground = Brushes.Red;
+                Alert.Foreground = Brushes.Red;
+
+            }
+            else
+            if (AgeValidation(userAge) == true)
+            {
+
+
+                if (resulty)
+                {
+                    Alert.Text = $"Witaj {userName}!";
+                    result.Text = "Użytkownik jest pełnoletni";
+                    result.Foreground = Brushes.Green;
+                    Alert.Foreground = Brushes.Green;
+
 
                 }
                 else
-                if (AgeValidation(userAge) == true)
                 {
-
-
-                    if (resulty)
-                    {
-                        Alert.Text = $"Witaj {userName}!";
-                        result.Text = "Użytkownik jest pełnoletni";
-                        result.Foreground = Brushes.Green;
-                        Alert.Foreground = Brushes.Green;
-
-
-                    }
-                    else
-                    {
-                        Alert.Text = $"Witaj {userName}!";
-                        result.Text = "Użytkownik nie jest pełnoletni";
-                        Alert.Foreground = Brushes.Green;
-                        result.Foreground = Brushes.Green;
-                    }
-
-
+                    Alert.Text = $"Witaj {userName}!";
+                    result.Text = "Użytkownik nie jest pełnoletni";
+                    Alert.Foreground = Brushes.Green;
+                    result.Foreground = Brushes.Green;
                 }
-            else 
+
+
+            }
+            else
             {
                 Alert.Text = " ";
                 result.Text = " ";
@@ -125,16 +131,14 @@ namespace Zadanie1WPF
                 Alert.Foreground = Brushes.Red;
 
 
-
-
-
             }
 
 
-
-
         }
-            public bool NameValidation(string userName)
+        #endregion
+
+        #region NameValidation
+        public bool NameValidation(string userName)
         {
             try
             {
@@ -164,21 +168,16 @@ namespace Zadanie1WPF
                 return false;
             }
         }
+        #endregion
 
-        //Validate age value
+        #region AgeValidation
         public bool AgeValidation(string age)
         {
             int ageInt;
 
-
-
-
             if (!int.TryParse(age, out ageInt))
             {
                 nullAge = false;
-
-
-                        
 
                 if (ageInt < 0 || ageInt > 150)
                 {
@@ -186,23 +185,15 @@ namespace Zadanie1WPF
                     {
                         if (age == "" || age == " ")
                         {
-
-
                             return false;
                         }
                         else if (c < '1' || c > '9')
                         {
-
-
                             return false;
                         }
                     }
-                    
+
                 }
-
-
-
-                
 
             }
             else
@@ -218,14 +209,15 @@ namespace Zadanie1WPF
 
 
                 return true;
+           
             }
 
 
             return false;
 
 
-
         }
+        #endregion
     }
 }
 
